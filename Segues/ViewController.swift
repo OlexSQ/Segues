76a8dad0854b62250c32e09e5a24f506a8d27675
Oleg <olegpsiarovsky@gmail.com>
2019-02-18 01:57:45 +0200

    Initial Commit

---
+++ Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by mac on 18.02.2019.
+//  Copyright © 2019 Oleg_Psiarovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
